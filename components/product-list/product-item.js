c843139c2e8fb84733352e25bd91c16ec410e10f
Albert Slyvester Duro <slyboydon1@gmail.com>
2021-12-29 14:11:56 +0100

    link to product detials page from special category products added

--- components/product-list/product-item.js
+++ components/product-list/product-item.js
@@ -6,8 +6,14 @@ function ProductItem({ product }) {
     const { id, name, discount, price, image } = product 
     
     const currentPrice = price - (price * (discount / 100))
+
+    async function addToCart(e) {
+        e.preventDefault()
+        console.log("added to cart")
+    }
+    
     return (
-        <Link href="#some-product">
+        <Link href={`/product/${id}`}>
             <a className="product-list__item">
                 <span className="product-list__item-discount">-{ discount }%</span>
                 <Image className="product-list__item-image" src={`/uploads/products/${image}`} alt={name} layout="responsive" height={300} width={200} />
@@ -16,7 +22,7 @@ function ProductItem({ product }) {
                     <h3 className="product-list__item-desc-name" title={ name.long }>{ name.short }</h3>
                     <span className="product-list__item-desc-price product-list__item-desc-price--prev">&#8358; { price }</span>
                     <span className="product-list__item-desc-price product-list__item-desc-price--curr">&#8358; { commaNumber(currentPrice) }</span>
-                    <button className="product-list__item-cart-btn" id={id}>
+                    <button className="product-list__item-cart-btn" onClick={addToCart} id={id}>
                         <span className="produt-list__item-cart-btn--text">Add to cart</span>
                         <i className="fas fa-cart-plus"></i>
                     </button>
